8ed4d9948106ce68d59c62564df158128dff620a
yuny0623 <overman522@gmail.com>
2022-11-23 15:41:49 +0900

    Add: jsonParser added to parse input from client.

---
+++ src/main/java/org/server/Utils/JsonGenerator.java
@@ -0,0 +1,11 @@
+package org.server.Utils;
+
+public class JsonGenerator {
+    public static String generateJson(){
+        return "";
+    }
+
+    public static void parseJson(){
+        
+    }
+}
