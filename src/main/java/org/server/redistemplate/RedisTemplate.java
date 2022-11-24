e215d0a5564c0808633eb8f34c8bef0a1eb2e464
yuny0623 <overman522@gmail.com>
2022-11-24 13:04:12 +0900

    Feat: RedisTemplate acts requested action.

---
+++ src/main/java/org/server/redistemplate/RedisTemplate.java
@@ -0,0 +1,32 @@
+package org.server.redistemplate;
+
+public class RedisTemplate {
+
+    public RedisTemplate(){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void attack(){
+
+    }
+
+    public void showMonsters(){
+
+    }
+
+    public void showUsers(){
+
+    }
+
+    public void chat(){
+
+    }
+
+    public void activateBot(){
+
+    }
+}
