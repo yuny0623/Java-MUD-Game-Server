27961000d5df5ffd2f8f981a964e939f06f14308
yuny0623 <overman522@gmail.com>
2022-12-04 19:30:06 +0900

    Test: add parseJson method test case.

--- src/test/java/JsonUtilTest.java
+++ src/test/java/JsonUtilTest.java
@@ -6,7 +6,18 @@ import org.server.utils.JsonUtil;
 public class JsonUtilTest {
     @Test
     public void parseJsonTest(){
+        // given
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("command", "move");
+        jsonObject.put("x", "1");
+        jsonObject.put("y", "2");
+        String json = jsonObject.toJSONString();
 
+        // when
+        String message = JsonUtil.parseJson(json);
+
+        // then
+        Assert.assertEquals(message, "move 1 2");
     }
 
     @Test
