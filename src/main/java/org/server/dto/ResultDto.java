4fc5955f925f196fa73251dfc442bd08a88d8e11
yuny0623 <overman522@gmail.com>
2022-12-07 13:46:27 +0900

    Add: Add ResultDto to return game play result

---
+++ src/main/java/org/server/dto/ResultDto.java
@@ -0,0 +1,14 @@
+package org.server.dto;
+
+public class ResultDto {
+    String command;
+    String result;
+
+    public String getCommand(){
+        return this.command;
+    }
+
+    public String getResult(){
+        return this.result;
+    }
+}
