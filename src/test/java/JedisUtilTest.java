25cc9de2d87f6f213e17ae075d62ddd1f5e40a63
yuny0623 <overman522@gmail.com>
2022-12-09 19:03:07 +0900

    test: Refactor test case

--- src/test/java/JedisUtilTest.java
+++ src/test/java/JedisUtilTest.java
@@ -3,6 +3,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.server.game.monster.MonsterManager;
+import org.server.utils.GameUtil;
 import org.server.utils.JedisUtil;
 import org.server.config.ServerConfig;
 import redis.clients.jedis.Jedis;
@@ -28,7 +30,7 @@ public class JedisUtilTest {
 
     @Test
     @DisplayName("jedis get 테스트")
-    public void jedisUtilTest(){
+    public void jedis_get_test(){
         // given
         String nickname = "monad";
         jedis.sadd("nicknames",  nickname);
@@ -43,51 +45,61 @@ public class JedisUtilTest {
         Assert.assertEquals(hp, "30");
         Assert.assertEquals(str, "3");
     }
-
+    
     @Test
     @DisplayName("유저 생성 테스트")
-    public void userCreationTest(){
+    public void user_creation_test(){
         // given
-        String nickname = "jenifer";
-        int x = (int) (Math.random() * (29 - 0) + 0) + 0;
-        int y = (int) (Math.random() * (29 - 0) + 0) + 0;
+        String nickname = "tony";
+        int x = GameUtil.generateRandomNumber(0, 29);
+        int y = GameUtil.generateRandomNumber(0, 29);
         jedis.sadd("nicknames", nickname);
-        jedis.set(nickname + ":hp", "30");
-        jedis.set(nickname + ":str", "3");
-        jedis.set(nickname + ":x", String.valueOf(x));
-        jedis.set(nickname + ":y", String.valueOf(y));
+        jedis.hset(nickname, "user_nickname", nickname);
+        jedis.hset(nickname, "hp", "30");
+        jedis.hset(nickname, "str", "3");
+        jedis.hset(nickname, "x_pos", String.valueOf(x));
+        jedis.hset(nickname, "y_pos", String.valueOf(y));
+        jedis.hset(nickname, "hp_potion", "1");
+        jedis.hset(nickname, "str_potion", "1");
+        jedis.expire(nickname, 5 * 60);
 
         // when
-        boolean isIn = jedis.sismember("nicknames", nickname);
-        String hp = jedis.get(nickname + ":hp");
-        String str = jedis.get(nickname + ":str");
-        String foundX = jedis.get(nickname + ":x");
-        String foundY = jedis.get(nickname + ":y");
+        boolean isMember = jedis.sismember("nicknames", "tony");
+        String foundNickname = jedis.hget("tony", "user_nickname");
+        String hp = jedis.hget("tony", "hp");
+        String str = jedis.hget("tony", "str");
+        String x_pos = jedis.hget("tony", "x_pos");
+        String y_pos = jedis.hget("tony", "y_pos");
+        String hp_potion = jedis.hget("tony", "hp_potion");
+        String str_potion = jedis.hget("tony", "str_potion");
 
         // then
-        Assert.assertTrue(isIn);
+        Assert.assertTrue(isMember);
+        Assert.assertEquals(nickname, foundNickname);
         Assert.assertEquals("30", hp);
         Assert.assertEquals("3", str);
-        Assert.assertEquals(String.valueOf(x), foundX);
-        Assert.assertEquals(String.valueOf(y), foundY);
+        Assert.assertEquals(String.valueOf(x), x_pos);
+        Assert.assertEquals(String.valueOf(y), y_pos);
+        Assert.assertEquals("1", hp_potion);
+        Assert.assertEquals("1", str_potion);
     }
 
     @Test
-    @DisplayName("showMonsters 메소드 테스트")
-    public void showMonstersTest(){
+    @DisplayName("생성된 몬스터들이 제한된 범위 내에 존재하는지 테스트")
+    public void created_monster_is_in_range_test(){
         // given
+        MonsterManager monsterManager = new MonsterManager();
+        monsterManager.start();
         String monsters = JedisUtil.showMonsters();
         String[] monsterRows = monsters.split("\n");
-        String[] vals=  null;
         List<Integer> posList = new ArrayList<>();
 
         // when
         for(int i = 0; i < monsterRows.length; i++){
-            vals = monsterRows[i].split(" ");
+            String[] vals = monsterRows[i].split(" ");
             posList.add(Integer.valueOf(vals[1]));
             posList.add(Integer.valueOf(vals[2]));
         }
-
         boolean isInRange = true;
         for(int i = 0; i < posList.size(); i++){
             if(posList.get(i) < 0 || posList.get(i) > 29){
@@ -96,7 +108,6 @@ public class JedisUtilTest {
         }
 
         // then
-        Assert.assertNotNull(vals);
         Assert.assertTrue(isInRange);
     }
 }
