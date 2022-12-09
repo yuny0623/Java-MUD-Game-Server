263abd0cfc843bba568438be5f4e935a58d2a9a6
yuny0623 <overman522@gmail.com>
2022-12-09 19:32:12 +0900

    feat: Add RestServer to create socket

--- src/main/java/org/server/http/RestServer.java
+++ src/main/java/org/server/http/RestServer.java
@@ -1,4 +1,41 @@
 package org.server.http;
 
-public class RestServer {
+import org.server.config.ServerConfig;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class RestServer extends Thread{
+
+    ServerSocket serverSocket;
+    Socket socket;
+    BufferedReader in;
+    String strIn;
+
+    public RestServer(){
+        System.out.println("[Rest Server] Start Rest Server.\n");
+    }
+
+    @Override
+    public void run(){
+        try{
+            serverSocket = new ServerSocket(ServerConfig.HTTP_CONNECTION_DEFAULT_PORT);
+            serverSocket.setReuseAddress(true);
+            while(true){
+                socket = serverSocket.accept();
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                strIn = in.readLine();
+                System.out.println(strIn);
+                /*
+                    소켓 처리
+                 */
+            }
+        }catch(IOException e)   {
+            e.printStackTrace();
+        }
+    }
+
 }
